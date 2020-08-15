790daf3d62c1c555569fffa3cb14273be72654b3
santosh0787 <santosh0787@gmail.com>
2020-08-15 17:25:39 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,9 +4,9 @@ terraform {
 }
 
 provider "azurerm" {
-  # whilst the `version` attribute is optional, we recommend pinning to a given version of the Provider
+ 
   version = "=2.0.0"
-  subscription_id = "0c3e0b25-bcd1-4f79-9a3a-cc0c892d6bfa"
-  tenant_id = "2c03f75f-06e4-46f7-86f5-d8d70bef1cf3"
+  subscription_id = "xxxxxxxxxxxxx"
+  tenant_id = "2xxxxxxxxxxxx"
   features {}
 }
